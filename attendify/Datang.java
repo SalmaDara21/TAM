334621d7d00733ef5a79909cf0897e990b09ff53
Sarah Rosniani <92279395+SarahRosniani@users.noreply.github.com>
2023-04-16 21:15:41 +0700

    Add files via upload

---
+++ attendify/Datang.java
@@ -0,0 +1,14 @@
+package com.sarah.attendify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Datang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datang);
+    }
+}
---
+++ attendify/perizinan.java
@@ -0,0 +1,14 @@
+package com.sarah.attendify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class perizinan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perizinan);
+    }
+}
